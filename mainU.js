2aa33bf9b8f802b64840d473186b379ddb01cd92
Anish Mankotia <amankotia146@gmail.com>
2019-09-09 22:09:47 +0530

    first commit

---
+++ mainU.js
@@ -0,0 +1,24 @@
+function handleClick(btnVal) {
+      var result = document.getElementById('result');
+      if(btnVal=== '='){
+      result.innerText = eval(result.innerText);
+
+
+}
+
+ else if (btnVal ==='C'){
+	result.innerText = ' ';
+
+
+}
+ 
+ else if(result.innerText==='0'){
+
+ 	result.innerText = btnVal;
+ }
+ else
+ {
+ 	result.innerText += btnVal;
+ }
+
+} 
